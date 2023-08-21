9e189bd4ed2c41120790e6e8e0ade004224c103c
Samarpit <nsamarpit25@gmail.com>
2023-08-21 19:58:50 +0530

    modify javascript to show level in heading

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ let compList = [];
 let usrList = [];
 let level = 0;
 for (let i = 0; i < buttons.length; i++) {
-
   $(buttons[i]).on("click", function () {
-
+    $("h1").text(
+      `Level:- ${level}, Continue to play!`
+    );
     // Add Class of pressed on cliclicking every button.
     usrList.push(buttons[i]);
     buttonPressed(buttons[i]);
@@ -21,44 +22,40 @@ $("body").on("keypress", function () {
     randomButton();
   }
 });
-$('#start').on('click', function () {
-    buttonPressed('#start')
-    if ((usrList = [])) {
-      setTimeout(randomButton, 500)
-      }
-})
+$("#start").on("click", function () {
+  buttonPressed("#start");
+  if ((usrList = [])) {
+    setTimeout(randomButton, 500);
+  }
+});
 
-
-
-function buttonPressed(btn) { 
-    $(btn).addClass("pressed");
+function buttonPressed(btn) {
+  $(btn).addClass("pressed");
   setTimeout(function () {
     $(btn).removeClass("pressed");
-  }, 100);
- }
+  }, 200);
+}
 // This function will automatically random button to computer List.
 function randomButton() {
   let randomIndex = Math.floor(Math.random() * buttons.length);
   compList.push(buttons[randomIndex]);
   buttonPressed(buttons[randomIndex]);
   level++;
-  usrList = [] 
+  usrList = [];
 }
 
 function checkUsrInput() {
   if (usrList.length < compList.length) {
     let slicedCompList = compList.slice(0, usrList.length);
     if (usrList.toString() === slicedCompList.toString()) {
-      $("h1").text("Continuie!!");
     } else {
-        GameOver()
+      GameOver();
     }
   } else if (usrList.length === compList.length) {
     if (usrList.toString === compList.toString) {
-      setTimeout(randomButton, 500)
+      setTimeout(randomButton, 1000);
     } else {
-      GameOver()
-      
+      GameOver();
     }
   }
 }
@@ -67,9 +64,12 @@ function checkUsrInput() {
 function GameOver() {
   usrList = [];
   compList = [];
+  level = 0;
   $("body").addClass("red");
-      setTimeout(() => {
-        $("body").removeClass("red");
-    }, 100);
-    $('h1').text(`Game over at level ${level}. Press any key or Start Button to start again`)
+  setTimeout(() => {
+    $("body").removeClass("red");
+  }, 100);
+  $("h1").text(
+    `Game over at level ${level}. Press any key or Start Button to start again`
+  );
 }
